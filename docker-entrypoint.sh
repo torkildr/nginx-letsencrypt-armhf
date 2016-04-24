2394b6c51d044e785b4d57aefbd62ab1a4875e9f
Gary Monson <gary.monson@gmail.com>
2016-04-24 23:32:55 +1000

    Check certificates on container startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-/configure-hosts.sh
+/update-certs
+
+echo "Starting nginx in the background..."
+nginx &
+sleep 2
+
+echo "Checking certs..."
+/update-certs
+
+echo "Stopping nginx..."
+nginx -s quit
+sleep 5
 
 echo "Starting nginx..."
 nginx -g "daemon off;"
